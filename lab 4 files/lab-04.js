d3771a0c56f00f4fe8f0cce02aadf4bb4f2ad9c0
nataleeirwin <83787587+nataleeirwin@users.noreply.github.com>
2021-06-02 14:10:06 -0700

    Create lab-04.js

---
+++ lab 4 files/lab-04.js
@@ -0,0 +1,37 @@
+// Require the Fastify framework and instantiate it
+const fastify = require("fastify")();
+// Handle GET verb for / route using Fastify
+// Note use of "chain" dot notation syntax
+fastify.get("/", (request, reply) => {
+  reply
+    .code(200)
+    .header("Content-Type", "text/html; charset=utf-8")
+    .send("<h1>Hello from Lab 4!</h1>");
+});
+
+//Route for /name
+fastify.get("/name", (request, reply) => {
+    
+    // (A) Get information from the request
+    const {first,last} = request.query;
+
+    // (B) Modify/transform/use information from the request in some way
+    const name = first && last ? `${first} ${last}` : `Guest`;
+
+    // (C) Return the result of our modification /transformation to the client
+    reply
+      .code(200)
+      .header("Content-Type", "text/html; charset=utf-8")
+      .send(`<h1>Hello, ${name}</h1>`);
+  });
+
+// Start server and listen to requests using Fastify
+const listenIP = "localhost";
+const listenPort = 8080;
+fastify.listen(listenPort, listenIP, (err, address) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  console.log(`Server listening on ${address}`);
+});
